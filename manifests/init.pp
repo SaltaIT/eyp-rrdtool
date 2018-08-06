929cde76415a239f95675441a166cd0985905b8d
Jordi Prats <jordi.prats@gmail.com>
2018-08-06 11:13:16 +0200

    lint + updated acceptance to centos 7 only

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
-class rrdtool(
-              $manage_package        = true,
-              $package_ensure        = 'installed',
-            ) inherits rrdtool::params{
+class rrdtool (
+                $manage_package = true,
+                $package_ensure = 'installed',
+              ) inherits rrdtool::params {
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
 
